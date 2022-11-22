cbc7066bc65d1ddb558587bddad3bc603ce12c8e
Peter Scheffler <p.scheffler@f5.com>
2022-11-22 10:18:41 -0500

    Added code to display the signature details Updated the README.md

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
+# List Signatures
+
+## Introduction
 Quick example python to spin thru a BIG-IP and pull the Policy ID(s)
 
 Then it iterates thru the results and shows the signatures and their Blocking state.
 
 This has no real error handling - like if the password is not right or if the policy list is empty...
 
-Added a couple new files:
+
+##Added a couple new files:
 
 policychangedetails.py - Outputs the last change of the selected policy
 policyauditdetails.py - Pulls the audit log for the selected policy/policies
+
+## November 22, 2022
+Added a call to the signature data in the loop ... note that this greatly increases the run time since we need to call them each time.  Might be good to have the Whole Signature DB pulled and then use a JSON query to get it but for now, this works as a Proof of Concept
+
--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from xml.etree.ElementInclude import include
 import requests
 import json
 import pprint
+import re 
+import urllib3
 
-bigip_host = "HostName_or_IP"
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+bigip_host = "BIG-IP.Host_or_IP"
 username = "admin"
-hiddenpassword = "SetYourPassword"
+hiddenpassword = "SuperSecretPassword"
 policyName = ""
 # Format is policyName = "?$filter=name eq policy-name"
 # Leave it blank and it will iterate thru all of the policies 
@@ -57,5 +61,16 @@ for policy in policyData['items']:
 
     sigs = json.loads(response.text)
     for sig in sigs['items']:
-        print(policy['name'] + " -- Signature: " + sig['signatureReference']['name'] + ":" + str(sig['signatureReference']['signatureId']) + " (blocking=" + str(sig['block']) + ")" )
+      # pull the signature reference link and parse out the ID from the end
+      fullSignatureUrl = sig['signatureReference']['link']
+      stepOne = re.split('[?]', fullSignatureUrl)
+      stepTwo = re.split('[/]', stepOne[0])
+      signatureId = stepTwo.pop()
 
+      sigurl  = "https://" + bigip_host + "/mgmt/tm/asm/signatures/" + signatureId
+
+      payload = ""
+      sigResponse = requests.request("GET", sigurl, headers=headers, data=payload, verify=False)
+      sigData = json.loads(sigResponse.text)
+
+      print(policy['name'] + " -- Signature: " + sig['signatureReference']['name'] + ":" + str(sig['signatureReference']['signatureId']) + ": Attack Type: " + str(sigData["attackTypeReference"]["name"]) + ": Accuracy: " + str(sigData["accuracy"]) + " (blocking=" + str(sig['block']) + ")" )
